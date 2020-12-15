8c213305b6b66447d62a444dacbfa5868d52baf0
AhmadRafif-AIO <73737874+AhmadRafif-AIO@users.noreply.github.com>
2020-12-15 13:57:00 +0700

    Tugas-Uas

---
+++ project_kel_/phpnya.php
@@ -0,0 +1,56 @@
+<html>
+	<head>
+		<title>Tugas WEB</title>
+		<link rel="stylesheet" type="text/css" href="cssnya.css">
+	</head>
+	<body>
+		<div class="wrapper">
+			<header>
+				<h1>Kelompok1.com</h1>
+				<p><i>The best trip advisor<i><p>
+					<nav>
+						<ul>
+							<li><a href="#">beranda</a></li>
+							<li><a href="#">rekomendasi</a></li>
+							<li><a href="#">kategori</a></li>
+							<li><a href="#">maps</a></li>
+							<li><a href="#">kontak</a></li>
+						</ul>
+					</nav>
+			</header>
+			<section class="courses">
+				<?php
+					echo "Welcome To Kelompok1.com";
+					echo "<br/>";
+					echo "You've Log In, ".$_GET["user"];
+				?>
+				<article>
+					<figure>
+						<img src="line_460775427153579.jpg" alt="Gede" width="100%"/>
+					</figure>
+					<hgroup>
+						<h2>Mount Gede</h2>
+						<h3>Jawa Barat</h3>
+					</hgroup>
+					<p>Gunung Gede merupakan sebuah gunung api bertipe stratovolcano. Gunung Gede berada dalam ruang lingkup Taman Nasional Gede Pangrango, yang merupakan salah satu dari lima taman nasional yang pertama kali diumumkan di Indonesia pada tahun 1980.</p>
+				</article>    
+				<article>
+					<figure>
+						<img src="MALANG_180414_0639.jpg" alt="bromo" width="100%"/>
+					</figure>
+					<hgroup>
+						<h2>Mount Bromo</h2>
+						<h3>Jawa Timur</h3>
+					</hgroup>
+						<p>Gunung Bromo atau dalam bahasa Tengger dieja "Brama". Gunung ini memiliki ketinggian 2.329 meter dan berada dalam empat wilayah kabupaten, yakni Kabupaten Probolinggo, Kabupaten Pasuruan, Kabupaten Lumajang, dan Kabupaten Malang.</p>
+				</article>
+			</section>
+			<footer>
+				@2020 Kelompok1.com
+			</footer>
+		</div>
+		
+		
+		
+	</body>
+</html>
